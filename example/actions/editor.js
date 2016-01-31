d4bfc28206d57bd293928aabe857340a98d469d9
Vjaceslavs Vasenins <time2type@gmail.com>
2016-01-31 17:22:07 +0200

    Moving from single object to inner object way

--- example/actions/editor.js
+++ example/actions/editor.js
@@ -2,7 +2,7 @@ var VcCake = require('vc-cake');
 VcCake.addAction('editor', function(app){
   var NavBar = app.getModule('navbar');
   app.getModule('editor').on('create', function(){
-    NavBar.init();
-  })
-  app.getModule('editor').init();
+    NavBar.invoke('init');
+  });
+  app.getModule('editor').invoke('init');
 }, ['navbar', 'editor']);
--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var curry = require('lodash/curry');
-var ModuleException = require('./lib/exception');
-var Parts = require('./lib/Parts');
-var Mediator = require('./lib/Mediator');
-var Stack = require('./lib/Stack');
+var moduleException = require('./lib/exception');
+var mediator = require('./lib/mediator');
+var Parts = require('./lib/parts');
+var Stack = require('./lib/stack');
 
 const SERVICE_TYPE = 'service';
 const MODULE_TYPE = 'module';
 const ACTION_TYPE = 'action';
 
 var Cake = {
-  add: function (type, name, callback, context, lazy, order) {
-    Parts.add(type + '-' + name, callback, context, lazy, order);
+  add: function (type, name, callback, context, order, wrapper) {
+    Parts.add(type + '-' + name, callback, context, order, wrapper);
   },
   subscribe: curry(function (type, name, eventName, callback) {
     return Mediator.subscribe(type + '-' + name + ':' + eventName, callback);
@@ -29,34 +29,47 @@ var GetBehaviorModule = function (modules) {
       Module.on = Cake.subscribe(MODULE_TYPE, name);
       return Module;
     }
-    throw new ModuleException('Behavior does not have an access to the module '
+    throw new moduleException('Behavior does not have an access to the module '
       + name);
   })(modules);
 };
 
-module.exports = {
-  addAction: function (name, callback, modules) {
-    Cake.add(ACTION_TYPE, name, callback, {
-      getService: this.getService,
-      getModule: new GetBehaviorModule(modules)
-    }, false, 6);
-    return this;
-  },
-  addModule: function (name, callback) {
-    Cake.add(MODULE_TYPE, name, callback, {
-      getService: this.getService,
-      publish: Cake.publish(MODULE_TYPE, name)
-    });
-    return this;
-  },
-  addService: function (name, callback) {
-    Cake.add(SERVICE_TYPE, name, callback, null, true, 4);
-    return this;
-  },
-  getService: function (name) {
-    return Cake.get(SERVICE_TYPE, name);
-  },
-  app: function () {
-    return new Stack();
+module.exports = function(){
+  var appMediator = new mediator();
+  return {
+    addAction: function (name, callback, modules) {
+      Cake.add(ACTION_TYPE, name, callback, {
+        getService: this.getService,
+        getModule: new GetBehaviorModule(modules)
+      }, 6, function(object){
+
+      });
+      return this;
+    },
+    addModule: function (name, callback) {
+      Cake.add(MODULE_TYPE, name, callback, {
+        getService: this.getService,
+        publish: Cake.publish(MODULE_TYPE, name)
+      });
+      return this;
+    },
+    addService: function (name, callback) {
+      Cake.add(SERVICE_TYPE, name, callback, null, 4, function(object){
+          return _.curry(function(object, load) {
+              return object;
+          })(object);
+      });
+      return this;
+    },
+    getService: function (name) {
+      return Cake.get(SERVICE_TYPE, name);
+    },
+    run: function () {
+      var app = new Stack();
+      app.init(function(){
+        Parts.load();
+      });
+      return app;
+    }
   }
 };
