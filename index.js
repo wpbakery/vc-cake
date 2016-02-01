8ef322787a042697c5f71374152a178792f39466
Vjaceslavs Vasenins <time2type@gmail.com>
2016-02-01 13:07:16 +0200

    Reloaction for dev

--- index.js
+++ index.js
@@ -1,60 +1,88 @@
 var curry = require('lodash/curry');
 var moduleException = require('./lib/exception');
-var mediator = require('./lib/mediator');
+var Mediator = require('./lib/mediator');
 var Parts = require('./lib/parts');
 var Stack = require('./lib/stack');
-
 const SERVICE_TYPE = 'service';
 const MODULE_TYPE = 'module';
 const ACTION_TYPE = 'action';
 
-var Cake = {
-  add: function (type, name, callback, context, order, wrapper) {
-    Parts.add(type + '-' + name, callback, context, order, wrapper);
-  },
-  subscribe: curry(function (type, name, eventName, callback) {
-    return Mediator.subscribe(type + '-' + name + ':' + eventName, callback);
-  }),
-  publish: curry(function (type, name, eventName, data) {
-    return Mediator.publish(type + '-' + name + ':' + eventName, data);
-  }),
-  get: function (type, name) {
-    return Parts.get(type + '-' + name);
+var apps = {};
+var envars = {
+  debug: false
+};
+var appException = function(message) {
+  if(true === envars.debug) {
+   throw new moduleException(message); 
   }
-};
-var GetBehaviorModule = function (modules) {
-  return curry(function(modules, name) {
-    if (modules.indexOf(name) > -1) {
-      var Module = Cake.get(MODULE_TYPE, name);
-      Module.on = Cake.subscribe(MODULE_TYPE, name);
-      return Module;
+}
+var Cake = {
+    add: function (parts,type, name, callback, context, order, wrapper) {
+      parts.add(type + '-' + name, callback, context, order, wrapper);
+    },
+    subscribe: curry(function (mediator, type, name, eventName, callback) {
+      return  mediator.subscribe(type + '-' + name + ':' + eventName, callback);
+    }),
+    publish: curry(function (mediator, type, name, eventName, data) {
+      return mediator.publish(type + '-' + name + ':' + eventName, data);
+    }),
+    get: function (parts, type, name) {
+      return parts.get(type + '-' + name);
     }
-    throw new moduleException('Behavior does not have an access to the module '
-      + name);
-  })(modules);
 };
-
-module.exports = function(){
-  var appMediator = new mediator();
+var GetBehaviorModule = function (parts, mediator, modules) {
+    return curry(function(modules, name) {
+      if (modules.indexOf(name) > -1) {
+        var Module = Cake.get(parts, MODULE_TYPE, name);
+        Module.on = Cake.subscribe(mediator, MODULE_TYPE, name);
+        return Module;
+      }
+      appException('Behavior does not have an access to the module '
+        + name);
+      return null;
+    })(modules);
+};
+exports.setEnv = function(key, value) {
+  envars[key] = value;
+};
+exports.env = function(key) {
+  return envars[key];
+};
+exports.app = function(name){
+  var appMediator = new Mediator();
+  var appParts = new Parts();
+  
   return {
     addAction: function (name, callback, modules) {
-      Cake.add(ACTION_TYPE, name, callback, {
+      Cake.add(appParts, ACTION_TYPE, name, callback, {
         getService: this.getService,
-        getModule: new GetBehaviorModule(modules)
-      }, 6, function(object){
+        getModule: new GetBehaviorModule(appParts, appMediator, modules)
+      }, 6, {
+          return {
+            invoke: _.curry(function(moduleName, name) {
+              if('undefind' !== typeof object[name]) {
+                var args = arguments.slice(1);
+                return object[name](...args);
+              })(name);
+              appException('Wrong method name "' + name + '" for module: ' + moduleName);
+              return null;
+            },
+            list: function() {
 
+            }
+          }
       });
       return this;
     },
     addModule: function (name, callback) {
-      Cake.add(MODULE_TYPE, name, callback, {
+      Cake.add(appParts, MODULE_TYPE, name, callback, {
         getService: this.getService,
-        publish: Cake.publish(MODULE_TYPE, name)
+        publish: Cake.publish(appMediator, MODULE_TYPE, name)
       });
       return this;
     },
     addService: function (name, callback) {
-      Cake.add(SERVICE_TYPE, name, callback, null, 4, function(object){
+      Cake.add(appParts, SERVICE_TYPE, name, callback, null, 4, function(object){
           return _.curry(function(object, load) {
               return object;
           })(object);
@@ -62,14 +90,22 @@ module.exports = function(){
       return this;
     },
     getService: function (name) {
-      return Cake.get(SERVICE_TYPE, name);
+      return Cake.get(appParts, SERVICE_TYPE, name);
     },
     run: function () {
-      var app = new Stack();
+      var app = new Stack(appMediator);
       app.init(function(){
-        Parts.load();
+        appParts.load();
       });
       return app;
     }
   }
+};
+
+exports.get = function(name) {
+  if('object' === typeof apps[name]) {
+    return apps[name];
+  }
+  appException('Wrong app name: ' + name);
+  return null;
 };
