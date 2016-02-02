1c9798c70e43c392312d4af0d76dca18cac41364
Vjaceslav Vasenin <time2type@gmail.com>
2016-02-02 17:31:28 +0200

    Logic changed modules became distributed

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var moduleException = require('./lib/exception');
 var Mediator = require('./lib/mediator');
 var Parts = require('./lib/parts');
 var Stack = require('./lib/stack');
+
 const SERVICE_TYPE = 'service';
-const MODULE_TYPE = 'module';
-const ACTION_TYPE = 'action';
 
 var apps = {};
 var envars = {
@@ -13,99 +12,70 @@ var envars = {
 };
 var appException = function(message) {
   if(true === envars.debug) {
-   throw new moduleException(message); 
+   throw new moduleException(message);
   }
+  return null;
 }
-var Cake = {
-    add: function (parts,type, name, callback, context, order, wrapper) {
-      parts.add(type + '-' + name, callback, context, order, wrapper);
-    },
-    subscribe: curry(function (mediator, type, name, eventName, callback) {
-      return  mediator.subscribe(type + '-' + name + ':' + eventName, callback);
-    }),
-    publish: curry(function (mediator, type, name, eventName, data) {
-      return mediator.publish(type + '-' + name + ':' + eventName, data);
-    }),
-    get: function (parts, type, name) {
-      return parts.get(type + '-' + name);
-    }
-};
-var GetBehaviorModule = function (parts, mediator, modules) {
-    return curry(function(modules, name) {
-      if (modules.indexOf(name) > -1) {
-        var Module = Cake.get(parts, MODULE_TYPE, name);
-        Module.on = Cake.subscribe(mediator, MODULE_TYPE, name);
-        return Module;
-      }
-      appException('Behavior does not have an access to the module '
-        + name);
-      return null;
-    })(modules);
-};
 exports.setEnv = function(key, value) {
   envars[key] = value;
 };
 exports.env = function(key) {
   return envars[key];
 };
-exports.app = function(name){
+var AppBuilder = function(name){
   var appMediator = new Mediator();
   var appParts = new Parts();
-  
-  return {
-    addAction: function (name, callback, modules) {
-      Cake.add(appParts, ACTION_TYPE, name, callback, {
-        getService: this.getService,
-        getModule: new GetBehaviorModule(appParts, appMediator, modules)
-      }, 6, {
-          return {
-            invoke: _.curry(function(moduleName, name) {
-              if('undefind' !== typeof object[name]) {
-                var args = arguments.slice(1);
-                return object[name](...args);
-              })(name);
-              appException('Wrong method name "' + name + '" for module: ' + moduleName);
-              return null;
-            },
-            list: function() {
+  var appModules = {};
+  var appName = name || '_';
+  if('undefined' !== typeof appName[name]) {
+    return appException('Application with this name ' + name + ' already exists');
+  }
+  // Create app object which will be
+  apps[name || '_'] = {
+    install: function(name, object) {
+      appModules[name] = {
+        on: function() {
 
-            }
-          }
+        }
+      };
+      return Object.create(object, {
+        notify: curry(function(){
+          return mediator.publish(name + ':' + eventName, data);
+        })(mediator, name)
       });
-      return this;
     },
-    addModule: function (name, callback) {
-      Cake.add(appParts, MODULE_TYPE, name, callback, {
-        getService: this.getService,
-        publish: Cake.publish(appMediator, MODULE_TYPE, name)
-      });
-      return this;
+    module: function(name) {
+      return modules[name] ?  : appException('Wrong module name: ' + name);
     },
+    getService: function (name) {
+      return parts.get(name);
+    }
+  };
+  return {
     addService: function (name, callback) {
-      Cake.add(appParts, SERVICE_TYPE, name, callback, null, 4, function(object){
+      parts.add(name, callback, null, 4, function(object){
           return _.curry(function(object, load) {
               return object;
           })(object);
       });
       return this;
     },
-    getService: function (name) {
-      return Cake.get(appParts, SERVICE_TYPE, name);
-    },
-    run: function () {
+    build: function () {
       var app = new Stack(appMediator);
       app.init(function(){
         appParts.load();
       });
       return app;
     }
-  }
+  };
 };
 
-exports.get = function(name) {
-  if('object' === typeof apps[name]) {
-    return apps[name];
+exports.app = function(name) {
+  var appName = name || '_'
+  if('object' === typeof apps[appName]) {
+    return apps[appName];
   }
   appException('Wrong app name: ' + name);
   return null;
 };
+module.exports = AppBuilder;
