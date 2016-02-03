b295ed64f4d79d438e393f1d275d6474cd7c8bc4
Vjaceslavs Vasenins <time2type@gmail.com>
2016-02-03 08:35:50 +0200

    Removed useless complexitiy

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
 var curry = require('lodash/curry');
 var moduleException = require('./lib/exception');
 var Mediator = require('./lib/mediator');
-var Parts = require('./lib/parts');
-var Stack = require('./lib/stack');
-
-const SERVICE_TYPE = 'service';
 
 var apps = {};
 var envars = {
   debug: false
 };
-var appException = function(message) {
+var appException = function(message, returnValue) {
   if(true === envars.debug) {
    throw new moduleException(message);
   }
-  return null;
+  return returnValue || null;
 }
-exports.setEnv = function(key, value) {
-  envars[key] = value;
-};
-exports.env = function(key) {
-  return envars[key];
-};
 var AppBuilder = function(name){
-  var appMediator = new Mediator();
-  var appParts = new Parts();
-  var appModules = {};
-  var appName = name || '_';
-  if('undefined' !== typeof appName[name]) {
-    return appException('Application with this name ' + name + ' already exists');
+  var data = {
+      mediator: new Mediator(),
+      modules: {},
+      services: {},
+      name: name || 'main';
+  };
+  if('string' !== typeof data.name || 'undefined' !== typeof apps[data.name]) {
+    return appException('Application with this name "' + name + '" already exists or name is wrong');
   }
   // Create app object which will be
-  apps[name || '_'] = {
+  apps[data.name] = {
     install: function(name, object) {
-      appModules[name] = {
-        on: function() {
-
-        }
+      data.modules[name] = {
+        on: curry(function(mediator, name, eventName, callback) {
+          return mediator.subscribe(name + ':' + eventName, callback);
+        })(data.mediator, name)
       };
       return Object.create(object, {
-        notify: curry(function(){
+        notify: curry(function(mediator, name, eventName, data){
           return mediator.publish(name + ':' + eventName, data);
-        })(mediator, name)
+        })(data.mediator, name)
       });
     },
     module: function(name) {
-      return modules[name] ?  : appException('Wrong module name: ' + name);
+      return modules[name] ? modules[name] : appException('Wrong module name: ' + name, {on: function(){
+        return this;
+      }});
     },
     getService: function (name) {
-      return parts.get(name);
+      return data.services[name];
+    },
+    // Global event subscibe
+    on: function(event, callback) {
+      data.mediator(event, callback);
     }
   };
   return {
     addService: function (name, callback) {
-      parts.add(name, callback, null, 4, function(object){
-          return _.curry(function(object, load) {
-              return object;
-          })(object);
-      });
+      data.services[name] = function(){
+        callback()
+      };
       return this;
     },
-    build: function () {
-      var app = new Stack(appMediator);
-      app.init(function(){
-        appParts.load();
-      });
-      return app;
+    start: function (callback) {
+      'function' === typeof callback && callback(this);
+      data.mediator.publish('start', true);
+      return this;
     }
   };
 };
-
-exports.app = function(name) {
-  var appName = name || '_'
+AppBuilder.app = function(name) {
+  var appName = name || 'main';
   if('object' === typeof apps[appName]) {
     return apps[appName];
   }
-  appException('Wrong app name: ' + name);
-  return null;
+  return appException('Wrong app name: ' + name);
 };
+AppBuilder.setEnv = function(key, value) {
+  envars[key] = value;
+};
+AppBuilder.env = function(key) {
+  return envars[key];
+};
+
 module.exports = AppBuilder;
