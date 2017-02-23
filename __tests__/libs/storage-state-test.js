325cd4869b3e21a267b5705ada78124e6e3c1a02
Vjaceslav Vasenin <time2type@gmail.com>
2017-02-23 18:51:34 +0200

    Add method test for storage state

--- __tests__/libs/storage-state-test.js
+++ __tests__/libs/storage-state-test.js
@@ -1,12 +1,12 @@
 /* global describe, it, expect */
 require('../_full_api_test')
-var methods = ['state', 'on', 'once', 'off', 'offOnce', 'trigger']
+var methods = ['set', 'get', 'onChange', 'ignoreChange']
 describe('check Events module methods', function () {
   methods.forEach(function (method) {
     it('checks method "' + method + '" exists', function () {
-      var Storage = require('../../lib/storage-constructor')
-      var testStorage = new Storage('test')
-      expect(testStorage[method]).toBeDefined()
+      var StorageState = require('../../lib/storage-state-constructor')
+      var testStorageState = new StorageState('test', 'stateState')
+      expect(testStorageState[method]).toBeDefined()
     })
   })
 })
