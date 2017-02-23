be2c5d3d82ee0bb8a0cf4a6415aaaa58c2d33912
Vjaceslav Vasenin <time2type@gmail.com>
2017-02-23 18:58:37 +0200

    Clean up test description. Add test to check is data stored in storage state

--- __tests__/libs/storage-state-test.js
+++ __tests__/libs/storage-state-test.js
@@ -1,12 +1,19 @@
 /* global describe, it, expect */
 require('../_full_api_test')
 var methods = ['set', 'get', 'onChange', 'ignoreChange']
-describe('check Events module methods', function () {
+describe('check Storage state module methods', function () {
   methods.forEach(function (method) {
     it('checks method "' + method + '" exists', function () {
       var StorageState = require('../../lib/storage-state-constructor')
       var testStorageState = new StorageState('test', 'stateState')
       expect(testStorageState[method]).toBeDefined()
     })
+  })
+  it('checks does data is stored', function () {
+    var StorageState = require('../../lib/storage-state-constructor')
+    var testStorageState = new StorageState('test', 'checking')
+    var testValue = 'check this value now'
+    testStorageState.set(testValue)
+    expect(testStorageState.get()).toBe(testValue)
   })
 })
--- __tests__/libs/storage-test.js
+++ __tests__/libs/storage-test.js
@@ -1,7 +1,7 @@
 /* global describe, it, expect */
 require('../_full_api_test')
 var methods = ['state', 'on', 'once', 'off', 'offOnce', 'trigger']
-describe('check Events module methods', function () {
+describe('check Storage methods', function () {
   methods.forEach(function (method) {
     it('checks method "' + method + '" exists', function () {
       var Storage = require('../../lib/storage-constructor')
