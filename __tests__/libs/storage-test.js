dcf6c83e59543524361809a16c4bce7877617e95
Vjaceslav Vasenin <time2type@gmail.com>
2018-04-10 13:30:04 +0300

    Add storage actions

--- __tests__/libs/storage-test.js
+++ __tests__/libs/storage-test.js
@@ -1,6 +1,6 @@
 /* global describe, it, expect */
 require('../_full_api_test')
-var methods = ['state', 'on', 'once', 'off', 'offOnce', 'trigger']
+var methods = ['state', 'on', 'once', 'off', 'offOnce', 'trigger', 'registerAction', 'action']
 describe('check Storage methods', function () {
   methods.forEach(function (method) {
     it('checks method "' + method + '" exists', function () {
